98aa276a9b512ae219df76a12e91326a8c0e5400
Daniel Bispo <daniel.bispo@bemobi.com.br>
2017-08-15 15:56:12 -0300

    Adicionando golang

---
+++ Go/macarena.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for macaCounter := 4; macaCounter > 0; macaCounter-- {
+		fmt.Println("Bailatuquerpwalegriamacarena!")
+		for i := 0; i < 3; i++ {
+			fmt.Println("bloblebloblablaiblablablioblablabuena")
+		}
+		eeeee := 3
+		for eeeee > 0 {
+			fmt.Println("eeeee")
+			eeeee--
+		}
+		fmt.Println("macarena!")
+		fmt.Println("AAAAAI!")
+	}
+}
